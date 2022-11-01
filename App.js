23c4d73c5d5cd2a24b120e747df8b9fc0c5e8fc9
Gianluca Bredice - Windows 10 Desktop <bredicegian@gmail.com>
2022-11-01 10:42:20 -0300

    1er desafio

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native"
+
+import { StatusBar } from "expo-status-bar"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hola, Coder!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 25,
   },
-});
+  text: {
+    color: "white",
+    backgroundColor: "black",
+    padding: 20,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+})
